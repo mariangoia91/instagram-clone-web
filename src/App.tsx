675d22b8dda07c2e1c6d02cfb734509b1be704c0
Marian Goia <marian.goia@principal33.com>
2024-07-15 17:15:14 +0300

    added bootstrap and folders

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -15,6 +17,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Button variant="primary">Primary</Button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
